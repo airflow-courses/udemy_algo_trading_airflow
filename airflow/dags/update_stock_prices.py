4d45c3def7a9bccd8192fd33ada9620dc73efe65
Your Name <you@example.com>
2021-02-17 08:23:15 +0300

    update

---
+++ airflow/dags/update_stock_prices.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from utils import db, tinkoff
+from datetime import timedelta
+import os
+
+DAG_ID = os.path.basename(__file__).replace('.pyc', '').replace('.py', '')
+CONN_ID = 'postgres_stocks'
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+with DAG(
+    dag_id=DAG_ID,
+    default_args=default_args,
+    schedule_interval='5 1 * * *',
+) as dag:
+
+    update_stock_prices_aapl = PythonOperator(
+        task_id='update_stock_prices_aapl',
+        python_callable=db.load_df_to_db,
+        op_kwargs={
+            'connector': CONN_ID,
+            'df': tinkoff.get_data_by_ticker_and_period(
+                'AAPL',
+                2,
+            ).tail(1),
+            'table_name': 'aapl',
+        }
+    )
