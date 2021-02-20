2299d1ca97b93349a5fcb0fbf17181e578515471
Your Name <you@example.com>
2021-02-20 23:29:17 +0300

    update

--- airflow/plugins/utils/strategy.py
+++ airflow/plugins/utils/strategy.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import numpy as np
 from utils import db
+from typing import Callable
+
+
+def apply_strategy(
+        connector: str,
+        source_table_name: str,
+        ticker: str,
+        strategy_func: Callable,
+        op_kwargs: dict
+) -> None:
+    data = db.get_data_from_table(connector, source_table_name)
+    signal = strategy_func(data, **op_kwargs).assign(ticker=ticker)
+    db.load_df_to_db(connector, signal, 'signal')
 
 
 def cross_sma_strategy(
