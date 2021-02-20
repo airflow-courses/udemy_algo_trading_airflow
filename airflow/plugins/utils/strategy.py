3a8aeecda71abf65aba22af418c9c0615778fc92
Your Name <you@example.com>
2021-02-20 22:53:24 +0300

    update

--- airflow/plugins/utils/strategy.py
+++ airflow/plugins/utils/strategy.py
@@ -14,3 +14,23 @@ def cross_sma_strategy(
     data['position'] = np.where(data['sma_short'] > data['sma_long'], 1, -1)
 
     return data[['time', 'position']].tail(1).assign(strategy_type='sma')
+
+
+def bollinger_bands_strategy(
+        data: pd.DataFrame,
+        sma: int,
+        dev: int,
+) -> pd.DataFrame:
+    data['sma'] = data['close'].rolling(sma).mean()
+    std = data['close'].rolling(SMA).std() * dev
+    data['lower'] = data['sma'] - std
+    data['upper'] = data['sma'] + std
+
+    data['distance'] = data['close'] - data['sma']
+    data['position'] = np.where(data['close'] < data['lower'], 1, np.nan)
+    data['position'] = np.where(data['close'] > data['upper'], -1, data['position'])
+    data['position'] = np.where(data['distance'] * data['distance'].shift(1) < 0,
+                                0, data['position'])
+    data['position'] = data['position'].ffill().fillna(0)
+
+    return data[['time', 'position']].tail(1).assign(strategy_type='bollinger')
